2e5fe21c36b8bf0373dc25159583233d4c0f53cd
scvsoft-cruzfernandez <cruz.fernandez@quizlet.com>
2024-01-11 17:51:48 -0300

    Fixed double quotes on generated script

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ sudo chsh -s $(which zsh) gitpod
 
 echo alias gitpod=/usr/bin/gp > $HOME/.oh-my-zsh/custom/gitpod.zsh
 
-echo alias cdq="cd /workspace/quizlet-web" > $HOME/.oh-my-zsh/custom/quizlet.zsh
+echo alias cdq=\"cd /workspace/quizlet-web\" > $HOME/.oh-my-zsh/custom/quizlet.zsh
 echo cdq >> $HOME/.oh-my-zsh/custom/quizlet.zsh
