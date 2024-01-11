4816c0400f27a605f79bd74264ee415dc1a977a1
scvsoft-cruzfernandez <cruz.fernandez@quizlet.com>
2024-01-11 17:48:27 -0300

    Added more customizations to mimmick normal bash env

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo chsh -s $(which zsh) gitpod
+
+echo alias gitpod=/usr/bin/gp > $HOME/.oh-my-zsh/custom/gitpod.zsh
+
+echo alias cdq="cd /workspace/quizlet-web" > $HOME/.oh-my-zsh/custom/quizlet.zsh
+echo cdq >> $HOME/.oh-my-zsh/custom/quizlet.zsh
