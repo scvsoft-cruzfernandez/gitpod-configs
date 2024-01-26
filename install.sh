761b6e808999f15a18135df3dab35d223e14e00c
scvsoft-cruzfernandez <cruz.fernandez@quizlet.com>
2024-01-26 11:49:34 -0300

    Added prompt.zsh as a standalone file

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ echo alias gitpod=/usr/bin/gp > $HOME/.oh-my-zsh/custom/gitpod.zsh
 echo alias cdq=\"cd /workspace/quizlet-web\" > $HOME/.oh-my-zsh/custom/quizlet.zsh
 echo cdq >> $HOME/.oh-my-zsh/custom/quizlet.zsh
 
-echo export PROMPT='%{$fg[white]%}%M %{$fg_bold[red]%}➜ %{$fg_bold[green]%}%p %{$fg[cyan]%}%c %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%} % %{$reset_color%}' >> $HOME/.oh-my-zsh/custom/prompt.zsh
+cp prompt.zsh $HOME/.oh-my-zsh/custom/prompt.zsh
