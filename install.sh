8c1d77513780ba682e0f6c1fe9bb758f56e4af24
scvsoft-cruzfernandez <cruz.fernandez@quizlet.com>
2024-02-26 11:13:24 -0300

    Added git submodule update automatically to switching branches, 'git pull'ing, etc.

--- install.sh
+++ install.sh
@@ -9,3 +9,4 @@ echo cdq >> $HOME/.oh-my-zsh/custom/quizlet.zsh
 echo export PROMPT=\''%{$fg[white]%}%M %{$fg_bold[red]%}➜ %{$fg_bold[green]%}%p %{$fg[cyan]%}%c %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%} % %{$reset_color%}'\' >> $HOME/.zshrc
 
 git config --global core.editor "vim"
+git config --global submodule.recurse true
