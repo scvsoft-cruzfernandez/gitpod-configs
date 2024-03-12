f8c523d410a1d190a8e46315de8bac4b165f855b
scvsoft-cruzfernandez <cruz.fernandez@quizlet.com>
2024-03-12 18:01:12 -0300

    Added some debugging, customization and printing yarn dev:links on startup

--- install.sh
+++ install.sh
@@ -2,13 +2,19 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 sudo chsh -s $(which zsh) gitpod
 
 echo alias gitpod=/usr/bin/gp > $HOME/.oh-my-zsh/custom/gitpod.zsh
+env
 
-echo alias cdq=\"cd /workspace/quizlet-web\" > $HOME/.oh-my-zsh/custom/quizlet.zsh
-echo cdq >> $HOME/.oh-my-zsh/custom/quizlet.zsh
+if [ $GITPOD_REPO_ROOT -eq "/workspace/quizlet-web" ]; then
+  echo alias cdq=\"cd /workspace/quizlet-web\" > $HOME/.oh-my-zsh/custom/quizlet.zsh
+  echo cdq >> $HOME/.oh-my-zsh/custom/quizlet.zsh
+  echo yarn dev:links >> $HOME/.oh-my-zsh/custom/quizlet.zsh
+fi
 
 echo export PROMPT=\''%{$fg[white]%}%M %{$fg_bold[red]%}➜ %{$fg_bold[green]%}%p %{$fg[cyan]%}%c %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%} % %{$reset_color%}'\' >> $HOME/.zshrc
 
+# this one is not working
 git config --global core.editor "vim"
+# this one YES!
 git config --global submodule.recurse true
 
 # we should avoid this one, it takes forever to start gitpods, until someone fixed the docker image git version
